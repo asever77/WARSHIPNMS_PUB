0768dd8186cc29a57e0d815e710a9fa9d8ca010d
gjwodud88 <gjwodud88@hansungei.co.kr>
2025-11-04 20:56:58 +0900

    gjwodud88 first-setting

---
+++ src/views/Deviceoperation/Devicecontrol.vue
@@ -0,0 +1,56 @@
+<template>
+    <div style="margin-left: 151px;width: 1182px;">
+        <div style="border: 1px solid #000000;">
+            {{ lang.title }}
+            <span style="margin-left: 300px;cursor: pointer;" @click="onClickView('/deviceoperation/firmwaremanage')">{{ lang.firmwaremanage }}</span>
+            <span style="margin-left: 10px;cursor: pointer;" @click="onClickView('/deviceoperation/firmwareupgrade')">{{ lang.firmwareupgrade }}</span>
+            <span style="margin-left: 10px;cursor: pointer;" @click="onClickView('/deviceoperation/devicecontrol')">{{ lang.devicecontrol }}</span>
+            <span style="margin-left: 10px;cursor: pointer;" @click="onClickView('/deviceoperation/resultinquiry')">{{ lang.resultinquiry }}</span>
+        </div>
+        <div>
+            장치제어 내용
+        </div>
+    </div>
+</template>
+
+<script>
+import router from "@/router";
+
+const ko = {
+    "title": "장치운영",
+    "firmwaremanage": "펌웨어관리",
+    "firmwareupgrade": "펌웨어업그레이드",
+    "devicecontrol": "장치제어",
+    "resultinquiry": "결과조회"
+}
+
+const en = {
+    "title": "장치운영",
+    "firmwaremanage": "펌웨어관리",
+    "firmwareupgrade": "펌웨어업그레이드",
+    "devicecontrol": "장치제어",
+    "resultinquiry": "결과조회"
+}
+
+export default {
+    name: "DeviceoperationDevicecontrolView",
+    data() {
+        return {
+            lang: {}
+        };
+    },
+    mounted() {
+        if(localStorage.getItem('lang') === 'ko') {
+            this.lang = ko;
+        }else {
+            this.lang = en;
+        }
+    },
+    methods: {
+        onClickView(item) {
+            router.push("/default").catch(() => {});
+            router.push(item);
+        }
+    },
+};
+</script>
---
+++ src/views/Documentmanage/Manual.vue
@@ -0,0 +1,47 @@
+<template>
+    <div style="margin-left: 151px;width: 1182px;">
+        <div style="border: 1px solid #000000;">
+            {{ lang.title }}
+            <span style="margin-left: 300px;cursor: pointer;" @click="onClickView('/documentmanage/manual')">{{ lang.manual }}</span>
+        </div>
+        <div>
+            매뉴얼 내용
+        </div>
+    </div>
+</template>
+
+<script>
+import router from "@/router";
+
+const ko = {
+    "title": "문서관리",
+    "manual": "매뉴얼"
+}
+
+const en = {
+    "title": "문서관리",
+    "manual": "매뉴얼"
+}
+
+export default {
+    name: "DocumentmanageManualView",
+    data() {
+        return {
+            lang: {}
+        };
+    },
+    mounted() {
+        if(localStorage.getItem('lang') === 'ko') {
+            this.lang = ko;
+        }else {
+            this.lang = en;
+        }
+    },
+    methods: {
+        onClickView(item) {
+            router.push("/default").catch(() => {});
+            router.push(item);
+        }
+    },
+};
+</script>
