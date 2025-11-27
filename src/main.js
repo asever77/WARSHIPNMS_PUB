ddaa8898b8b186680532d6fc59d6c50efd3f8116
kangjy <mizushiro@naver.coom>
2025-11-27 16:56:46 +0900

    css 순서 변경

--- src/main.js
+++ src/main.js
@@ -5,10 +5,10 @@ import App from './App.vue'
 import router from './router'
 
 // Bootstrap 및 BootstrapVueNext import
-import './assets/global.css';
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js' // 이 라인을 추가합니다.
 import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+import './assets/global.css';
 
 // Custom directives
 import BsTooltip from './directives/v-bs-tooltip'
