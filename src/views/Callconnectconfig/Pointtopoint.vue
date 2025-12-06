0b90abbac3d2175ee8f9dd5b87d148a0342c08c4
mizushiro <39608160+mizushiro@users.noreply.github.com>
2025-12-06 18:30:43 +0900

    통화연결설정 테이블 진행중

--- src/views/Callconnectconfig/Pointtopoint.vue
+++ src/views/Callconnectconfig/Pointtopoint.vue
@@ -1,6 +1,21 @@
 <template>
-    <div>
-        {{ lang.title }}
+    <div class="ui-excel-table">
+      <table class="table-type-b">
+        <thead>
+        <tr>
+          <th></th>
+          <th scope="col"><span>{{ lang.thead1 }}</span></th>
+          <th scope="col"><span>{{ lang.thead2 }}</span></th>
+          <th scope="col" v-for="n in 40" :key="n"><span>{{ lang.thead1 }}</span></th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="n in 20" :key="n">
+          <th scope="col">{{ lang.tbody1 }}</th>
+          <td v-for="n in 42" :key="n"></td>
+        </tr>
+        </tbody>
+      </table>
     </div>
 </template>
 
@@ -9,11 +24,14 @@ import { ref, onMounted } from "vue";
 import G from "@/config/global.js";
 
 const ko = {
-  "title": "점대점 내용"
+  thead1: "BRIDGE 1/CAPTAIN(S)",
+  thead2: "BRDG 2 /CONNING",
+  tbody1: "BRIDGE 1/CAPTAIN(S)"
 };
 
 const en = {
-  "title": "점대점 내용"
+  thead1: "BRIDGE 1/CAPTAIN(S)",
+  tbody1: "BRIDGE 1/CAPTAIN(S)"
 };
 
 const lang = ref({});
