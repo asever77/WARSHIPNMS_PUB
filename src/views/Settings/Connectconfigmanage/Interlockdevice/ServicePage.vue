4087c68e0859fa0af9fd2a7cb092ae25b590c8f1
kangjy <mizushiro@naver.coom>
2025-12-15 17:21:58 +0900

    서비스 설정

--- src/views/Settings/Connectconfigmanage/Interlockdevice/ServicePage.vue
+++ src/views/Settings/Connectconfigmanage/Interlockdevice/ServicePage.vue
@@ -1,22 +1,76 @@
 <template>
-    <div>
-       서비스
+  <div class="base-wrap">
+    <div class="search-base">
+      <div class="search-base--form">
+        <BFormSelect
+          id="search-target"
+          class="ui-select-28"
+          :options="[{ value: '1', text: '보안장비연동장치' }]"
+        ></BFormSelect>
+      </div>
+      <div class="search-base--btns">
+        <BFormGroup :label="lang.label1" label-for="search-deviceType">
+          <BFormSelect
+            id="search-deviceType"
+            class="ui-select-28 min-w-400"
+            :options="[{ value: '1', text: '사용자단말 <-> MRU-09  <-> UHF NB' }]"
+          />
+        </BFormGroup>
+      </div>
     </div>
+  </div>
+  <div class="ui-drag-area1">
+    <div class="ui-drag-section">
+      <div class="ui-drag-section-item" data-bg="sky">
+        <div class="ui-drag-item" data-bg="sky">사용자단말</div>
+      </div>
+      <i class="icon-connection-status" data-connection="true"></i>
+      <div class="ui-drag-section-item" data-bg="yellow">
+
+      </div>
+      <i class="icon-connection-status" data-connection="false"></i>
+      <div class="ui-drag-section-item" data-bg="pink">
+        UHF(T) #1
+      </div>
+    </div>
+    <div class="ui-drag-item-section">
+      <div class="ui-drag-item-group">
+        <div class="ui-drag-item-grid">
+          <div class="ui-drag-item" data-bg="sky" v-for="n in 16" :key="n">사용자단말</div>
+        </div>
+      </div>
+      <div class="ui-drag-item-group">
+        <div class="ui-drag-item-grid">
+          <div class="ui-drag-item" data-bg="yellow" v-for="n in 6" :key="n">사용자단말</div>
+        </div>
+      </div>
+      <div class="ui-drag-item-group">
+        <div class="ui-drag-item-grid">
+          <div class="ui-drag-item" data-bg="pink" v-for="n in 6" :key="n">사용자단말</div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="ui-btn-group">
+    <BButton class="blue28">{{ lang.btn1 }}</BButton>
+  </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
-import G from "@/config/global.js";
-
+import { ref, onMounted, computed, watch, reactive } from 'vue'
+import G from '@/config/global.js'
+import { BFormSelect } from 'bootstrap-vue-next/components'
+import { BButton, BFormGroup, BPagination, BTable } from 'bootstrap-vue-next'
 
 const ko = {
-  "title": "보안장비연동장치 내용"
-};
+  btn1: '적용',
+  label1: '템플릿 운영모드',
+}
 
-const en = {
-  "title": "보안장비연동장치 내용"
-};
+const en = {}
 
-const lang = ref({});
-
+const lang = ref({})
+onMounted(() => {
+  lang.value = G.lang === 'ko' ? ko : en
+})
 </script>
