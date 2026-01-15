12bdd8fcf4c067e9aa9d6bb355d13b7acc96aa78
asever77 <johyunmin77@gmail.com>
2026-01-15 10:32:12 +0900

    L2스위치 팝업 화면 수정 요청

--- src/components/PortStatusList.vue
+++ src/components/PortStatusList.vue
@@ -1,9 +1,39 @@
 <template>
-  <ul class="port-status">
-    <li v-for="port in ports" :key="port.number" :data-status="port.status">
-      <b>{{ port.number }}</b>
-    </li>
-  </ul>
+  <div class="port-status-wrap">
+    <ul class="port-status">
+      <li data-status="normal"><b>01</b></li>
+      <li data-status="abnormal"><b>02</b></li>
+      <li><b>03</b></li>
+      <li><b>04</b></li>
+      <li><b>05</b></li>
+      <li><b>06</b></li>
+      <li><b>07</b></li>
+      <li><b>08</b></li>
+      <li><b>09</b></li>
+      <li><b>10</b></li>
+      <li><b>11</b></li>
+      <li><b>12</b></li>
+      <li><b>13</b></li>
+      <li><b>14</b></li>
+      <li><b>15</b></li>
+      <li><b>16</b></li>
+      <li><b>17</b></li>
+      <li><b>18</b></li>
+      <li><b>19</b></li>
+      <li><b>20</b></li>
+      <li><b>21</b></li>
+      <li><b>22</b></li>
+      <li><b>23</b></li>
+      <li><b>24</b></li>
+    </ul>
+
+    <ul class="port-status group-port">
+      <li data-status="normal"><b>G1</b></li>
+      <li data-status="abnormal"><b>G2</b></li>
+      <li><b>G3</b></li>
+      <li><b>G4</b></li>
+    </ul>
+  </div>
 </template>
 
 <script setup>
@@ -20,15 +50,27 @@ const props = defineProps({
 </script>
 
 <style scoped>
+.port-status-wrap {
+  display: flex;
+  justify-content: center;
+  align-items:end;
+  gap: 4rem;
+  margin: 2rem auto;
+}
+.group-port.port-status {
+  flex-direction: row;
+  height: 2rem;
+}
+.group-port.port-status li b{
+  transform: translateY(2rem);
+}
 .port-status{
-  display: inline-flex;
-  flex-direction: column-reverse;
-  height: 4.4rem;
+  display: flex;
+  flex-direction: column;
   flex-wrap: wrap;
   gap:.2rem;
-  margin: 2rem auto;
   width: auto;
-
+  height: 4.2rem;
 }
 .port-status li{
   position: relative;
@@ -43,15 +85,12 @@ const props = defineProps({
   align-items: center;
   justify-content: center;
   height: 2rem;
-  transform: translateY(2rem);
+  transform: translateY(-2rem);
 }
 .port-status li:nth-child(even) b{
-   transform: translateY(-2rem);
+   transform: translateY(2rem);
 }
-.port-status li:nth-child(25),
-.port-status li:nth-child(26){
-  margin-left: 2rem;
-}
+
 .port-status li[data-status="normal"]{background-color: #4BC43D;}
 .port-status li[data-status="abnormal"]{background-color: #F27322;}
 </style>
